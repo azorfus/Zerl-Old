47c513756b20da785bfb99092dcd5d9703c82cff
azorfus <72391760+azorfus@users.noreply.github.com>
2021-04-19 21:35:13 +0530

    Update lex.c

--- src/lex.c
+++ src/lex.c
@@ -13,15 +13,15 @@ enum tokentype
 	TYPE_LOOP,
 	TYPE_IF,
 	TYPE_ELSE,
-    TYPE_EQ,
-    TYPE_ASGN,
-    TYPE_GRT,
-    TYPE_LSS,
-    TYPE_NE,
-    TYPE_ADD,
-    TYPE_SUB,
-    TYPE_MUL,
-    TYPE_DIV
+	TYPE_EQ,
+	TYPE_ASGN,
+	TYPE_GRT,
+	TYPE_LSS,
+	TYPE_NE,
+	TYPE_ADD,
+	TYPE_SUB,
+	TYPE_MUL,
+	TYPE_DIV
 };
 
 typedef struct token_lex
@@ -40,7 +40,7 @@ char* block;
 
 void parse(int i, int j)
 {
-	// wTOK.type_str = TypeToString(TYPE_STR); TODO
+	// wTOK.type_str = TypeToString(TYPE_STR); todo
 	printf("[%d] %d, %d - %s\n", wTOK.type, i, j, wTOK.token);
 }
 
@@ -59,28 +59,42 @@ char* substr(char* source, int start, int end)
 	return retstr;
 }
 
+// main function that tokenizes the source code.
 void tokenize_code(char* source_code)
 {
 	size_t source_len = strlen(source_code);
-	int end_quote = 0;
-	int end_int = 0;
 	for(int i=0;i<source_len;i++)
 	{
-		if(source_code[i]=='\"' && i!=end_quote)
+		if(source_code[i]=='"')
 		{
 			for(int j=i+1;j<source_len;j++)
 			{
-				if(source_code[j]=='\"')
+				if(source_code[j]=='\\')
 				{
-					end_quote=j;
-					wTOK.token = substr(source_code, i, j);
+					switch(source_code[j+1])
+					{
+						case '"':
+							j+=2;
+							break;
+						case '\\':
+							j+=2;
+							break;
+					
+						default:
+							continue;
+					}
+				}
+				if(source_code[j]=='"')
+				{
+					wTOK.token = substr(source_code, i, j+1);
 					wTOK.type = TYPE_STR;
 					parse(i, j+1);
+					i = j;
 					break;
 				}
 			}
 		}
-		else if(source_code[i] >= '0' && source_code[i] <= '9' && i > end_int && i > end_quote)
+		else if(source_code[i] >= '0' && source_code[i] <= '9')
 		{
 			for(size_t j=i;j<source_len;j++)
 			{
@@ -88,11 +102,23 @@ void tokenize_code(char* source_code)
 				{
 					wTOK.token = substr(source_code, i, j);
 					wTOK.type = TYPE_INT;
-					end_int = j;
 					parse(i, j);
+					i = j;
 					break;
 				}
 			}
+		}
+		else if(source_code[i]=='(')
+		{
+					wTOK.type = TYPE_OP;
+					wTOK.token = "(";
+					parse(i, i+1);
+		}
+		else if(source_code[i]==')')
+		{
+					wTOK.type = TYPE_CP;
+					wTOK.token = ")";
+					parse(i, i+1);
 		}
 	}
 }
