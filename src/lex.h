0b8a55be165a2c3f8e2dc8f75f9e1ede32e24793
azorfus <72391760+azorfus@users.noreply.github.com>
2021-04-22 11:39:26 +0530

    Update lex.h

--- src/lex.h
+++ src/lex.h
@@ -1,12 +1,10 @@
 #ifndef LEX_H
 #define LEX_H
-
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include "lex.h"
+#include <stdlib.h>
+#include <string.h>
 
-void tokenize_code(char* source_code);
+void tokenize(char* src);
 
 #endif
