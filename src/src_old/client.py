d563a9388fde64c9a21de83f22e7ef95824dbfc2
azorfus <72391760+azorfus@users.noreply.github.com>
2023-10-29 21:39:10 +0530

    Add files via upload

---
+++ src/src_old/client.py
@@ -0,0 +1,218 @@
+'''
+
+	Written by Azorfus
+	Github: @azorfus
+
+'''
+
+import socket
+import threading
+import time
+from datetime import datetime
+import os
+
+SERVER_HOST = "192.168.1.11" # str(input("Enter Host IP: "))
+SERVER_PORT = 8080
+separator_token = "<SEP>"
+
+fsport = 0
+
+s = socket.socket()
+
+# event = threading.Event()
+# event.set()
+
+try:
+	s.connect((SERVER_HOST, SERVER_PORT))
+	fsport = int(s.recv(256).decode().split("!@$:")[0])
+	print("File transport port: ", fsport)
+
+except ConnectionRefusedError:
+	print("Server is not available. Exiting...")
+	exit(1)
+
+print(f"[*] Connected to {SERVER_HOST}:{SERVER_PORT}.")
+
+name = input("Enter your name: ")
+date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+conn_init = f"[{date_now}] {separator_token} {name} has connected.!@$:"
+s.send(conn_init.ljust(1024, "#").encode())
+
+def listen_for_messages():
+	while True:
+		#if event.is_set():
+		msg_R = s.recv(1024).decode('utf-8', errors='ignore')
+		msg = msg_R.split("!@$:")[0]
+		if msg == "server!abort!code!abort":
+			s.close()
+			break
+		if msg != "file!download!incoming":
+			print("\n" + msg)
+		#else:
+		#	print("Paused Thread")
+		#	event.wait()
+		
+
+def file_transfer(file_name):
+	print("\n[*] Establishing tunnel to server, this may take a moment...")
+
+	# We wait for server to open its FTP server and send confirmation that its open so that
+	# we can connect.
+	confirmation = s.recv(1024).decode()
+	confirmation = confirmation.split("!@$:")[0]
+
+	# If the confirmation isn't as expected, we wait on a loop listening to the server for
+	# the confirmation.
+	while confirmation != "file!tunnel!open":
+		confirmation = s.send("uncon!@$:".ljust(1024, "#").encode())
+
+		confirmation = s.recv(1024).decode()
+		confirmation = confirmation.split("!@$:")[0]
+
+	# If confirmation is received we send a pong message back to the server to confirm 
+	# the received confirmation.
+	print("[!] Received confirmation from server... Connecting...")
+	s.send("PONG!@$:".ljust(1024, "#").encode())
+
+	fs = socket.socket()
+	try:
+		fs.connect((SERVER_HOST, fsport))
+		print("\n[*] Uploading file via tunnel to server... This will take a moment...")
+		file = open(file_name, "rb")
+		file_size = os.path.getsize(file_name)
+		rem = int(file_size % 1024)
+		quo = int((file_size - rem)/1024)
+		packet_info = str(quo) + ":@!$" + str(rem) + ":@!$" + file_name + ":@!$" + name + ":@!$"
+		send_packet = packet_info.ljust(256, "#")
+		fs.send(send_packet.encode())
+
+		# packet transmission
+		fs.sendall(file.read())
+
+		file.close()
+
+	except Exception as e:
+		print(f"[!] Could not establish file transfer tunnel.\nError: {e}")
+		fs.close()
+		return 0
+
+	try:
+		confirmation = fs.recv(1024).decode()
+		confirmation = confirmation.split("!@$:")[0]
+	
+		# If the confirmation isn't as expected, we wait on a loop listening to the server for
+		# the confirmation.
+		while confirmation != "file!upload!success":
+			print("[*] Waiting for upload success confirmation....")
+			confirmation = fs.send("uncon!@$:".ljust(1024, "#").encode())
+			confirmation = fs.recv(1024).decode()
+
+		print(f"[*] File transfer tunnel successfully closing... File transfer was successful.")
+	
+	except Exception as e:
+		print(f"[!] File transfer failed, Data was corrupted or was not sent through.\nConfirmation not received from server.\nError: {e}")
+
+	fs.close()
+	return 0
+
+def file_download(file_name):
+	print("\n[*] Establishing tunnel to server, this may take a moment...")
+
+
+	# We wait for server to open its FTP server and send confirmation that its open so that
+	# we can connect.
+	confirmation = s.recv(1024).decode()
+	confirmation = confirmation.split("!@$:")[0]
+
+	# If the confirmation isn't as expected, we wait on a loop listening to the server for
+	# the confirmation.
+	while confirmation != "file!tunnel!open":
+		confirmation = s.send("uncon!@$:".ljust(1024, "#").encode())
+
+		confirmation = s.recv(1024).decode()
+		confirmation = confirmation.split("!@$:")[0]
+
+	# If confirmation is received we send a pong message back to the server to confirm 
+	# the received confirmation.
+	print("[!] Received confirmation from server... Connecting...")
+	s.send("PONG!@$:".ljust(1024, "#").encode())
+	
+	fs = socket.socket()
+	try:
+		fs.connect((SERVER_HOST, fsport))
+		print(f"[*] Established tunnel to server on port: {fsport}")
+		packet_info_R = fs.recv(256)
+		packet_info = packet_info_R.decode('utf-8', errors='ignore')
+		packet_struct = packet_info.split(":@!")
+	
+		rem = int(packet_struct[1])
+		quo = int(packet_struct[0])
+		req_file = packet_struct[2]
+	
+		with open(req_file, "wb") as f:
+	
+			data = bytearray()
+			count = 0
+			packet = 0
+			while count <= quo:
+				if count == quo:
+					packet = fs.recv(rem)
+				else:
+					packet = fs.recv(1024)
+		
+				data += packet
+				count += 1
+		
+			f.write(data)
+			f.close()
+		print("[*] Closing file transfer tunnel...")
+		
+		done = "file!download!success!@$:"
+		done = done.ljust(1024, "#")
+		fs.send(done.encode())
+		fs.close()
+
+		print("File downloaded...", f"file name: {packet_struct[2]}, size: {quo * 4096 + rem} bytes.\n")
+
+	except Exception as e:
+		print(f"[!] Could not establish file transfer tunnel.\nError: {e}")
+		fs.close()
+		return 0
+
+	return 0
+
+lisn = threading.Thread(target = listen_for_messages)
+lisn.daemon = True
+lisn.start()
+
+while True:
+	date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	to_send = input()
+
+	try:
+		if to_send.lower() == 'quit':
+			to_send = "client!exit!code!@$:"
+			s.send(to_send.ljust(1024, "#").encode())
+			break
+	
+		elif to_send.strip() == "!upload":
+			fileName = input("Enter file name: ")
+			to_send = f"file!transfer!code!@$:{fsport}!@$:"
+			s.send(to_send.ljust(1024, "#").encode())
+			file_transfer(fileName)
+	
+		elif to_send.strip() == "!download":
+			fileName = input("Enter <user>__<file name>: ")
+			to_send = f"file!download!request!@$:{fsport}!@$:" + fileName + "!@$:"
+			s.send(to_send.ljust(1024, "#").encode())
+			file_download(fileName)
+	
+		else:
+			to_send = f"[{date_now}] {name}{separator_token}{to_send}!@$:"
+			s.send(to_send.ljust(1024, "#").encode())
+		
+	except Exception as e:
+		print(f"[!] Error: {e}")
+
+s.close()
+
---
+++ src/src_old/server.py
@@ -0,0 +1,238 @@
+'''
+
+	Written by Azorfus
+	Github: @azorfus
+
+'''
+
+import socket
+import threading
+import time
+import os
+
+host = "192.168.1.11" # str(input("Enter Host IP: "))
+port = 8080
+sep_tok = "<SEP>"
+MAX_LISTEN_TIME = 2
+exit_flag = threading.Event()
+
+client_sockets = []
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host, port))
+s.listen(20)
+
+print(f"[*] Listening as {host}:{port}")
+
+def send_file(cs, file_name):
+	try:
+		file = open(file_name, "rb")
+		file_size = os.path.getsize(file_name)
+
+		rem = int(file_size % 1024)
+		quo = int((file_size - rem)/1024)
+		file_name_back = file_name.split("__")[1]
+	
+		# Packet with file size and name information.
+		packet_info = str(quo) + ":@!" + str(rem) + ":@!" + file_name_back + ":@!"
+		send_packet = packet_info.ljust(256, "#")
+		cs.send(send_packet.encode())
+		cs.sendall(file.read())
+		
+		file.close()
+
+	except Exception as e:
+		print(f"[!] Error: {e}")
+		return 0
+
+	return 0
+	
+def recv_file(kcs):
+	cs_ip = kcs.getpeername()
+
+	packet_info = kcs.recv(256).decode('utf-8', errors='ignore')
+	packet_struct = packet_info.split(":@!$")
+
+	rem = int(packet_struct[1])
+	quo = int(packet_struct[0])
+
+	with open(f"{packet_struct[3]}__{packet_struct[2]}", "wb") as file:
+
+		count = 0
+		packet = 0
+		while count <= quo:
+			if count == quo:
+				packet = kcs.recv(rem)
+			else:
+				packet = kcs.recv(1024)
+				print("Receiving... ", count*1024)
+			file.write(packet)
+			count += 1
+
+		file.close()
+
+	now = time.time()
+	pong_recv = ""
+
+	while pong_recv != "PONG":		
+		if time.time() > now + 5:
+			print("Timed out FTS...")
+			break
+
+		confirmation = "file!upload!success!@$:"
+		confirmation = confirmation.ljust(1024, "#")
+		kcs.send(confirmation.encode())
+		
+		pong_recv = kcs.recv(1024).decode()
+		pong_recv = pong_recv.split("!@$:")[0]
+
+	upload_info = f"{packet_struct[3]} uploaded file {packet_struct[2]} size: {quo*4096+rem} bytes.\nType \"!download\" and\n then enter the file name in the format <username>__<file name>\ni.e. \"{packet_struct[3]}__{packet_struct[2]}\" to download the file.\n"
+	for client_socket in client_sockets:
+		client_socket.send(upload_info.encode())
+	
+	return 0
+
+def establish_conn(cs, file_name, FPORT, FLAG):
+
+	# Start time of the server
+	start_time = time.time()
+
+	fs = socket.socket()
+	fs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	fs.bind((host, fport))
+	fs.settimeout(120)
+	print("[*] Listening for file tunnel connection...")
+
+	# Listen for file tunnel connections within the time limit
+	fs.listen(5)
+	print("[*] Receiving connection requests to FTP server...")
+
+	now = time.time()
+	pong_recv = ""
+
+	while pong_recv != "PONG":		
+		if time.time() > now + 5:
+			print("Timed out FTS...")
+			return 0
+
+		confirmation = "file!tunnel!open!@$:"
+		confirmation = confirmation.ljust(1024, "#")
+		cs.send(confirmation.encode())
+		
+		pong_recv = cs.recv(1024).decode()
+		pong_recv = pong_recv.split("!@$:")[0]
+
+	print("[!] Received Pong message from client.")
+	
+	while (time.time() - start_time) <= MAX_LISTEN_TIME:
+		try:
+
+			if FLAG == "DOWN":
+				fcs, fcaddr = fs.accept()
+				print(f"[*] File transfer tunnel established with {fcaddr}")
+				send_file(fcs, file_name)
+				print(f"[*] File transfer tunnel successfully closing...")
+				break
+				
+				confirmation = s.recv(1024).decode()
+				confirmation = confirmation.split("!@$:")[0]
+			
+				while confirmation != "file!download!success":
+					time.sleep(2)
+					print("[*] Waiting for download to finish....")
+					confirmation = s.recv(1024).decode()
+					confirmation = confirmation.split("!@$:")[0]
+
+				fcs.close()
+				fs.close()
+
+			elif FLAG == "UP":	
+				fcs, fcaddr = fs.accept()
+				print(f"[*] File transfer tunnel established with {fcaddr}")
+				recv_file(fcs)
+				print(f"[*] File transfer tunnel successfully closing...")
+				fcs.close()
+				fs.close()
+				break
+
+		except Exception as e:
+			if e == socket.timeout:
+				print("Timing out...")
+				pass  # Ignore timeout and continue listening
+			
+			elif e == ConnectionResetError:
+				print("BRUH")
+				pass
+
+	return 0
+
+def listen_for_client(cs):
+	while not exit_flag.is_set():
+		try:	
+			msg_R = cs.recv(1024).decode('utf-8', errors='ignore')
+			msg = msg_R.split("!@$:")[0]
+
+			if msg == "client!exit!code":
+				print(f"[*] Client: {cs.getpeername()[0]} has closed the connection.")
+				client_sockets.remove(cs)
+				cs.close()
+				break
+
+			elif msg == "file!transfer!code":
+				FPORT = int(msg_R.split("!@$:")[1])
+				connThread = threading.Thread(target = establish_conn, args = (cs, "", FPORT, "UP" ))
+				connThread.daemon = True
+				connThread.start()
+
+			elif msg.split("!@$:")[0] == "file!download!request":
+				FPORT = int(msg_R.split("!@$:")[1])
+				file_name = msg_R.split("!@$:")[2]
+				connThread = threading.Thread(target = establish_conn, args = (cs, file_name, FPORT, "DOWN" ))
+				connThread.daemon = True
+				connThread.start()
+
+			else:
+				msg_R = msg.replace(sep_tok, ": ") + "!@$:"
+				msg = msg_R.ljust(1024, "#")
+				for client_socket in client_sockets:
+					client_socket.send(msg.encode())
+
+		except Exception as e:
+			print(f"[!] Error: {e}")
+			client_sockets.remove(cs)
+			cs.close()
+			break
+
+def server_term():
+	while not exit_flag.is_set():
+		cmd = input("$ ")
+		if cmd == "ABORT":
+			exit_flag.set()
+			for i in threading.enumerate():
+				i.join()
+			for cs in client_sockets:
+				abort_code = "server!abort!code!abort!@$:".ljust(1024, "#")
+				cs.send(abort_code.encode())
+				cs.close()
+			s.close()
+			break 
+
+
+term = threading.Thread(target = server_term)
+term.daemon = True
+term.start()
+
+while True:
+	client_socket, client_address = s.accept()
+	print(f"[+] {client_address} connected.")
+	client_sockets.append(client_socket)
+
+	fport = 8081 + client_sockets.index(client_socket)
+	sport = f"{fport}!@$:".ljust(256, "#")
+	client_socket.send(sport.encode())
+
+	client_t = threading.Thread(target=listen_for_client, args=(client_socket,))
+	client_t.daemon = True
+	client_t.start()
+
+s.close()
